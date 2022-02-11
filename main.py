243406e80587e14b8e81aee9dc5386b0e9537361
AakashS <69010695+Aakash-kun@users.noreply.github.com>
2022-02-11 12:00:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -54,27 +54,5 @@ async def my_startup_function():
             exc = f"{type(e).__name__,}: {e}"
             print(f"Failed to load extension {extension}\n{exc}")
 
-    # bot vars
-    bot.prefix = config.prefix
-    bot.home_server = bot.get_guild(int(config.home_server))
-    bot.guild_logger = await bot.fetch_channel(config.guild_logger)
-    bot.message_logger = await bot.fetch_channel(config.message_logger)
-    bot.owner_id = config.owner_id
-    bot.appeal_server_invite = config.appeal_server_invite
-    bot.main_server_invite = config.main_server_invite
-    bot.afk_ignored_channels = []
-    bot.session = aiohttp.ClientSession()
-    bot.listening_channels = config.crosstalk
-    bot.kucoin_api_key = config.kucoin_api_key
-    bot.kucoin_api_secret = config.kucoin_api_secret
-    bot.kucoin_api_passphrase = config.kucoin_api_passphrase
-
-    # dbs
-    bot.db = await aiosqlite.connect("dbs/db.sqlite3")
-    await bot.db.execute(
-        "CREATE TABLE IF NOT EXISTS afk (id bigint PRIMARY KEY, msg str)"
-    )
-
-
 bot.loop.create_task(my_startup_function())
 bot.run(config.token)
